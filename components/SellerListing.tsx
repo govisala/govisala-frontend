f0abe3401e378e0a515340f42f7346f760eb69a9
Sharunee Rajendren <sharuneeraj2000@gmail.com>
2025-02-06 14:56:12 +0530

    Added SellerListing page

--- components/SellerListing.tsx
+++ components/SellerListing.tsx
@@ -19,99 +19,97 @@ function SellerListing() {
 
   return (
     <SafeAreaView>
-      <Center>
-        <Box className="bg-[#C0D85F] h-full rounded-t-[40px] px-4">
-          <center>
-            <Text className="text-4xl font-p600 mt-8 p-1">Seller Listing</Text>
-            <Pressable className="w-30 h-30 rounded-full bg-[#D9D9D9] justify-center items-center"></Pressable>
-            <Input className="bg-[#FCFFE0] rounded-full mt-8 h-16" size={"xl"}>
-              <InputField
-                type="text"
-                placeholder="Item Name"
-                value={ItemName}
-                onChangeText={(text) => setitemName(text)}
-                className="font-p400"
-              />
-            </Input>
+      <Box className="bg-[#C0D85F] h-full rounded-t-[40px] px-4">
+        <Center>
+          <Text className="text-4xl font-p600 mt-8 p-1">Seller Listing</Text>
+          <Pressable className="w-30 h-30 rounded-full bg-[#D9D9D9] justify-center items-center"></Pressable>
+          <Input className="bg-[#FCFFE0] rounded-full mt-8 h-16" size={"xl"}>
+            <InputField
+              type="text"
+              placeholder="Item Name"
+              value={ItemName}
+              onChangeText={(text) => setitemName(text)}
+              className="font-p400"
+            />
+          </Input>
 
-            <Input className="bg-[#FCFFE0] rounded-full mt-8 h-16" size={"xl"}>
-              <InputField
-                type="text"
-                placeholder="Quantity"
-                value={Quantity}
-                onChangeText={(text) => setQuantity(text)}
-                className="font-p400"
-              />
-            </Input>
+          <Input className="bg-[#FCFFE0] rounded-full mt-8 h-16" size={"xl"}>
+            <InputField
+              type="text"
+              placeholder="Quantity"
+              value={Quantity}
+              onChangeText={(text) => setQuantity(text)}
+              className="font-p400"
+            />
+          </Input>
 
-            <Input className="bg-[#FCFFE0] rounded-full mt-8 h-16" size={"xl"}>
-              <InputField
-                type="text"
-                placeholder="Unit Price"
-                value={UnitPrice}
-                onChangeText={(text) => setUnitPrice(text)}
-                className="font-p400"
-              />
-            </Input>
+          <Input className="bg-[#FCFFE0] rounded-full mt-8 h-16" size={"xl"}>
+            <InputField
+              type="text"
+              placeholder="Unit Price"
+              value={UnitPrice}
+              onChangeText={(text) => setUnitPrice(text)}
+              className="font-p400"
+            />
+          </Input>
 
-            <Input className="bg-[#FCFFE0] rounded-full mt-8 h-16" size={"xl"}>
-              <InputField
-                type="text"
-                placeholder="Quality Grade"
-                value={Quality}
-                onChangeText={(text) => setQuality(text)}
-                className="font-p400"
-              />
-            </Input>
+          <Input className="bg-[#FCFFE0] rounded-full mt-8 h-16" size={"xl"}>
+            <InputField
+              type="text"
+              placeholder="Quality Grade"
+              value={Quality}
+              onChangeText={(text) => setQuality(text)}
+              className="font-p400"
+            />
+          </Input>
 
-            <Input className="bg-[#FCFFE0] rounded-full mt-8 h-16" size={"xl"}>
-              <InputField
-                type="text"
-                placeholder="Location"
-                value={Location}
-                onChangeText={(text) => setLocation(text)}
-                className="font-p400"
-              />
-            </Input>
+          <Input className="bg-[#FCFFE0] rounded-full mt-8 h-16" size={"xl"}>
+            <InputField
+              type="text"
+              placeholder="Location"
+              value={Location}
+              onChangeText={(text) => setLocation(text)}
+              className="font-p400"
+            />
+          </Input>
 
-            <Input className="bg-[#FCFFE0] rounded-full mt-8 h-16" size={"xl"}>
-              <InputField
-                type="text"
-                placeholder="Area"
-                value={Area}
-                onChangeText={(text) => setArea(text)}
-                className="font-p400"
-              />
-            </Input>
+          <Input className="bg-[#FCFFE0] rounded-full mt-8 h-16" size={"xl"}>
+            <InputField
+              type="text"
+              placeholder="Area"
+              value={Area}
+              onChangeText={(text) => setArea(text)}
+              className="font-p400"
+            />
+          </Input>
 
-            <Input className="bg-[#FCFFE0] rounded-full mt-8 h-16" size={"xl"}>
-              <InputField
-                type="text"
-                placeholder="Effective Date"
-                value={EffectiveDate}
-                onChangeText={(text) => setEffectiveDate(text)}
-                className="font-p400"
-              />
-            </Input>
+          <Input className="bg-[#FCFFE0] rounded-full mt-8 h-16" size={"xl"}>
+            <InputField
+              type="text"
+              placeholder="Effective Date"
+              value={EffectiveDate}
+              onChangeText={(text) => setEffectiveDate(text)}
+              className="font-p400"
+            />
+          </Input>
 
-            <Input className="bg-[#FCFFE0] rounded-full mt-8 h-16" size={"xl"}>
-              <InputField
-                type="text"
-                placeholder="Delivery or Pickup"
-                value={DeliveryPickup}
-                onChangeText={(text) => setDeliveryPickup(text)}
-                className="font-p400"
-              />
-            </Input>
+          <Input className="bg-[#FCFFE0] rounded-full mt-8 h-16" size={"xl"}>
+            <InputField
+              type="text"
+              placeholder="Delivery or Pickup"
+              value={DeliveryPickup}
+              onChangeText={(text) => setDeliveryPickup(text)}
+              className="font-p400"
+            />
+          </Input>
 
-            <Button className="bg-[#4E7456] h-14 rounded-full">
-              <ButtonText className="font-p500 text-[#FCFFE0] text-2xl">
-                Add List
-              </ButtonText>
-            </Button>
-          </center>
-        </Box>
-      </Center>
+          <Button className="bg-[#4E7456] h-14 rounded-full">
+            <ButtonText className="font-p500 text-[#FCFFE0] text-2xl">
+              Add List
+            </ButtonText>
+          </Button>
+        </Center>
+      </Box>
     </SafeAreaView>
   );
 }
